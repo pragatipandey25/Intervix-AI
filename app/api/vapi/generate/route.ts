2f9a4a241fc5823da913366eacf2569aa11a56e2
Pragati Pandey <pragatipandey454@gmail.com>
2026-06-25 18:46:26 +0530

    POST request added

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -41,8 +41,8 @@ export async function POST(request: Request) {
 
     return Response.json({ success: true }, { status: 200 });
   } catch (error) {
-    console.error("Error:", error);
-    return Response.json({ success: false, error: error }, { status: 500 });
+    console.error(error);
+    return Response.json({ success: false, error }, { status: 500 });
   }
 }
 
